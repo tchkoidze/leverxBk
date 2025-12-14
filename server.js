a35a79d92332e3ac734292d89f950b2eba078f1b
tchkoidze <khvicha.tchkoidze@toedur.com>
2025-12-14 23:35:31 +0400

    fix

--- server.js
+++ server.js
@@ -216,4 +216,8 @@ app.patch("/users/:id", async (req, res) => {
   return res.json({ message: "User updated", user });
 });
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+// app.listen(3000, () => console.log("Server running on http://localhost:3000"));
